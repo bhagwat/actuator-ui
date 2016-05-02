956c03f8e79e70ddcef3512dde5dcdffca0114d7
Bhagwat Kumar <bhagwat@intelligrape.com>
2016-05-02 16:52:25 +0530

    Refactor mappings page to use new design

--- src/app/controllers/MappingsController.js
+++ src/app/controllers/MappingsController.js
@@ -2,15 +2,37 @@
     angular
         .module('app')
         .controller('MappingsController', [
-            'ActuatorService', 'lodash',
-            function MappingsController(ActuatorService, lodash) {
+            'ActuatorService', '$scope', '$filter', 'lodash',
+            function MappingsController(ActuatorService, $scope, $filter, lodash) {
                 var vm = this;
 
-                vm.init = function () {
-                    ActuatorService.get({endpoint: 'mappings'}, function (response) {
-                        vm.mappings = response;
-                        vm.totalMappings = lodash.keys(response).length - 2; //exclude "$promise", "$resolved"
+                function formatResult(result) {
+                    delete result.$promise;
+                    delete result.$resolved;
+                    result = lodash.map(result, function (value, key) {
+                        value.uri = key;
+                        return value;
                     });
+                    $scope.result = result;
+                    $scope.total = $scope.result.length;
+                    $scope.sortAndPaginateResult();
+                }
+
+                $scope.selected = [];
+
+                $scope.query = {
+                    order: '-uri',
+                    limit: 10,
+                    page: 1
+                };
+
+                $scope.sortAndPaginateResult = function () {
+                    var orderedRecords = $filter('orderBy')($scope.result, $scope.query.order);
+                    $scope.paginatedResult = $filter('offset')(orderedRecords, $scope.query.page, $scope.query.limit);
+                };
+
+                $scope.init = function (endpoint) {
+                    $scope.promise = ActuatorService.get({endpoint: endpoint}, formatResult).$promise;
                 };
             }
         ]);
