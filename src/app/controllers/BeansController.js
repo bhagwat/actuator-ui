21e42bb3792c051a93f4c50dec8e67ba04e3d832
Bhagwat Kumar <bhagwat@intelligrape.com>
2016-05-02 16:34:08 +0530

    redesigned beans page to use better table design

--- src/app/controllers/BeansController.js
+++ src/app/controllers/BeansController.js
@@ -2,14 +2,31 @@
     angular
         .module('app')
         .controller('BeansController', [
-            'ActuatorService',
-            function BeansController(ActuatorService) {
+            'ActuatorService', '$scope', '$filter',
+            function BeansController(ActuatorService, $scope, $filter) {
                 var vm = this;
 
-                vm.init = function () {
-                    ActuatorService.query({endpoint: 'beans'}, function (response) {
-                        vm.contexts = response;
-                    });
+                function formatResult(result) {
+                    $scope.result = result[0].beans;
+                    $scope.total = $scope.result.length;
+                    $scope.sortAndPaginateResult();
+                }
+
+                $scope.selected = [];
+
+                $scope.query = {
+                    order: '-responseTime',
+                    limit: 10,
+                    page: 1
+                };
+
+                $scope.sortAndPaginateResult = function () {
+                    var orderedRecords = $filter('orderBy')($scope.result, $scope.query.order);
+                    $scope.paginatedResult = $filter('offset')(orderedRecords, $scope.query.page, $scope.query.limit);
+                };
+
+                $scope.init = function (endpoint) {
+                    $scope.promise = ActuatorService.query({endpoint: endpoint}, formatResult).$promise;
                 };
             }
         ]);
